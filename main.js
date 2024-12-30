400471c85b1c918e739feac83475f9519edeb5cb
jayesh-023 <133695747+jayesh-023@users.noreply.github.com>
2024-12-31 03:52:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 import { Alchemy, Network } from "alchemy-sdk";
 
 const config = {
-  apiKey: "qI467FddCE5eb_sKKb9aBZSX0wQ6ukzG",
+  apiKey: "< YOUR ALCHEMY API KEY >",
   network: Network.ETH_MAINNET,
 };
 const alchemy = new Alchemy(config);
 
 const main = async () => {
   // Get all NFTs
-  const nfts = await alchemy.nft.getNftsForOwner("0xd9e80B0EE8c4c5149A9c47A163940D02099d3E03");
+  const nfts = await alchemy.nft.getNftsForOwner("< WALLET ADDRESS >");
   // Print NFTs
   console.log(nfts);
 };
